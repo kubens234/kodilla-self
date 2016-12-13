f77fdcc61511a284eddcf70fd22e021e9d64a0f1
jakub <mroczkowski.jakub84@gmail.com>
2016-12-13 12:00:32 +0100

    poprawione

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ function Button(text) {
 
 Button.prototype = {
 	create: function() {
-	var self = this;
-	this.$element = $('<button>');
-	this.$element.text(this.text);
-	this.$element.click(function() {
-		alert(self.text);
-	});
-	$('body').append(this.$element);
-}
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
 }
 
 var btn1 = new Button('Hello!');
